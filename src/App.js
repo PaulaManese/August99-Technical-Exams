5a232010020759350a3bed2adc4f46ec527e14a1
Paula Manese <paulamanese@gmail.com>
2022-09-16 13:24:37 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import './App.css'
 
-function App() {
+const AsyncAwait = () => {
+  const [data, setData] = useState([] || null === 'No Data')
+
+  const fetchData = async () => {
+    const response = await fetch("https://api.spacexdata.com/v4/launches/")
+    const data = await response.json()
+    setData(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [setData])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.length > 0 && (
+        <div className="container">
+          <form>
+            <input type="search" class="searchbox" name="search" id="search" placeholder="Enter keywords..." />
+          </form>
+          <div className="retrievedata">
+            {data.map(item => (
+              <div className="itemByitem" key={item.id}>
+                <div>
+                  <img src={item.links.patch.small} loading="lazy" alt='spacexlaunch' />
+                </div>
+                <div className="info">
+                  <h4>Flight Number: {item.flight_number} {item.name} ({item.date_local})</h4>
+                  <small>Details: {item.details == null ? 'No Data' : item.details}</small>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default AsyncAwait
